5d3fb40123fe8ea6d42f624bc2da9a305cc323b9
Daníel Þór Calvi <daniel.thor.calvi@advania.is>
2025-03-03 13:37:00 +0000

    refactor and style changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,105 +1,13 @@
 import './App.css'
-import { useState, useEffect } from 'react'
-import { Grid2, Button, Box } from '@mui/material';
+import Game from './components/Game'
 
 function App() {
-  const alphabet: string = "ABCDEÉFGHIÍJKLMNOÓPQRSTUÚVWXYÝZÞÆÖ"
-  const wordString: string = "DOG"
-  const [word, _] = useState<string[]>(wordString.split(""))
-  const [displayWord, setDisplayWord] = useState<string>("")
-  const [guessCount, setGuessCount] = useState<number>(0);
-  const [guesses, setGuesses] = useState<string[]>([]);
-  const [win, setWin] = useState<boolean>(false);
-  const [loss, setLoss] = useState<boolean>(false);
 
-  useEffect(() => {
-    let temp = ""
-    word.forEach(letter => {
-      if(alphabet.includes(letter)){
-        temp += "_"
-      }
-      else {
-        temp += letter
-      }
-    })
-    setDisplayWord(temp)
-    return () => {
-      console.log("Component unmounted!");
-    };
-  }, []);
-
-  useEffect(() => {
-    if(guessCount === 6){
-      setLoss(true)
-    }
-  }, [guessCount])
-
-  const handleGuess = (letter: string) => {
-    if(loss || win) return; 
-    if(!guesses.includes(letter)) { 
-      setGuesses((prevGuesses) => [...prevGuesses, letter])   
-      if(word.includes(letter)) {
-        showLetter(letter)       
-      }
-      else{
-        setGuessCount(guessCount + 1)       
-      }
-    }
-  }
-
-  const showLetter = (letter: string) => {
-    let temp = displayWord.split("")
-    word.forEach((l, i) => {
-      if (l === letter) {
-        temp[i]= letter
-      }
-    });
-    setDisplayWord(temp.join(""))
-    console.log(temp, word)
-    if(word.join("") === temp.join("")){
-      setWin(true)
-    }
-  }
   
   return (  
     <>
-      <h1>Hengimann</h1>
-      {win && <h2>Þú vannst! 🥳</h2>}
-      {loss && <h2>Þú tapaðir! 💩</h2>}
-      <Box 
-        component="img"
-        src={`/${guessCount}.jpg`} 
-        alt="Random"
-        sx={{
-          width: "100%",
-          maxWidth: 500,
-          height: "auto",
-          borderRadius: 2,
-          boxShadow: 3,
-        }}
-      />
-      <h2 style={{letterSpacing: 5}}>{displayWord}</h2>
-      <h1>{guessCount}</h1>
-      <div className="card">
-        <Grid2 
-          container 
-          spacing={1}
-        >
-        {alphabet.split("").map(letter => (
-          <Grid2>
-            <Button 
-              variant="contained"
-              color="primary"
-              fullWidth
-              disabled={guesses.includes(letter)}
-              onClick={() => handleGuess(letter)}
-            >
-              {letter}
-            </Button>
-          </Grid2>
-        ))}
-        </Grid2>
-      </div>
+      <h1>Hangman</h1>
+      <Game />
     </>
   )
 }
---
+++ src/components/Game.tsx
@@ -0,0 +1,144 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+import { Grid2, Button, Box, Dialog, DialogTitle, DialogContent, DialogContentText, LinearProgress } from '@mui/material';
+function Game() {
+  const alphabet: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const [word, setWord] = useState<string[]>([])
+  const [displayWord, setDisplayWord] = useState<string>("")
+  const [guessCount, setGuessCount] = useState<number>(0);
+  const [guesses, setGuesses] = useState<string[]>([]);
+  const [win, setWin] = useState<boolean>(false);
+  const [loss, setLoss] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const gameOver = loss || win;
+
+  useEffect(() => { 
+    return () => {
+      init()   
+    }
+  }, []);
+
+  useEffect(() => {
+    if(guessCount === 6){
+      setLoss(true)
+    }
+  }, [guessCount])
+
+  const handleGuess = (letter: string) => {
+    if(loss || win) return; 
+    if(!guesses.includes(letter)) { 
+      setGuesses((prevGuesses) => [...prevGuesses, letter])   
+      if(word.includes(letter)) {
+        showLetter(letter)       
+      }
+      else{
+        setGuessCount(guessCount + 1)       
+      }
+    }
+  }
+
+  const showLetter = (letter: string) => {
+    let temp = displayWord.split("")
+    word.forEach((l, i) => {
+      if (l === letter) {
+        temp[i]= letter
+      }
+    });
+    setDisplayWord(temp.join(""))
+    console.log(temp, word)
+    if(word.join("") === temp.join("")){
+      setWin(true)
+    }
+  }
+
+  const init = async () => {
+    setIsLoading(true)
+    const response = await axios.get("https://random-word-api.herokuapp.com/word")
+    let word: string[] = response.data[0].toUpperCase().split("")
+    setWord(word)
+    let temp = ""
+    for(var letter of word) {
+      if(alphabet.includes(letter)){
+        temp += "_"
+      }
+      else {
+        temp += letter
+      }
+    }
+    setDisplayWord(temp)
+    setIsLoading(false)
+  }
+
+  const restart = () => {
+    setWin(false)
+    setLoss(false)
+    setGuessCount(0)
+    setGuesses([])
+    setDisplayWord("")
+    init()
+  }
+  
+  return (
+    <>
+      <Box 
+        component="img"
+        src={`/${guessCount}.jpg`} 
+        alt="Random"
+        sx={{
+        width: "300",
+        maxWidth: 350,
+        height: "auto",
+        borderRadius: 2,
+        boxShadow: 3,
+        }}
+      />
+      <div>
+      {isLoading ? ( <LinearProgress />) 
+      : <h1 style={{letterSpacing: 5}}>{displayWord}</h1>       
+      }
+      </div>
+      <div className="card">
+        <Grid2 
+        container 
+        spacing={0.5}
+        columns={13}
+        >
+        {alphabet.split("").map(letter => (
+          <Grid2 size={{xs: 4, sm: 2, md: 1}}>
+              <Button 
+                variant="contained"
+                color="primary"
+                fullWidth
+                disabled={guesses.includes(letter)}
+                onClick={() => handleGuess(letter)}
+              >
+              {letter}
+              </Button>
+          </Grid2>
+        ))}
+        </Grid2>
+      </div>
+      <Dialog open={gameOver} >
+        <DialogTitle align='center'>{win ? "Winner!" : "Loser"}</DialogTitle>
+        <DialogContent>
+          <Grid2 container>
+            <Grid2 justifyItems={'center'} size={12} >
+              <div style={{fontSize: 100}}>{win ? "🥳" : "💩"} </div>
+            </Grid2>
+          </Grid2>
+          <DialogContentText>
+            The word was <strong>{word.join("")}</strong>
+          </DialogContentText>
+        </DialogContent>
+        <Button 
+          onClick={restart}
+          variant="contained"
+          color="primary"
+          fullWidth>
+          Play again
+        </Button>
+      </Dialog>
+    </>
+  )
+}
+export default Game
